0f16eb2f5ad6e47ca52a4e5b0ef2c0f4949fb806
Ueslei <ueslei@outlook.com>
2024-03-31 18:09:53 -0300

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -7,6 +7,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 import pickle
+import os
 
 # Função para carregar os componentes necessários
 def load_components(model_path, tokenizer_path, label_encoder_path):
@@ -19,36 +20,23 @@ def load_components(model_path, tokenizer_path, label_encoder_path):
 
 # Função de normalização do texto
 def normalize_text(text):
-    text = text.lower()
-    text = re.sub(r'\[.*?\]', '', text)
-    text = re.sub(r'https?://\S+|www\.\S+', '', text)
-    text = re.sub(r'<.*?>+', '', text)
-    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub(r'\n', '', text)
-    text = re.sub(r'\w*\d\w*', '', text)
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = re.sub(r'\d+', '', text)
-    text = re.sub(r'[^a-záéíóúàèìòùâêîôûãõäëïöüç\s]', '', text)
-    text = re.sub(r'\s+', ' ', text).strip()
-    return text
+    # Sua função de normalização aqui
+    pass
 
 # Função para fazer predições
 def make_prediction(model, tokenizer, label_encoder, text):
-    max_length = 200  # Deve corresponder ao valor usado durante o treinamento
-    text = normalize_text(text)
-    sequence = tokenizer.texts_to_sequences([text])
-    padded_sequence = pad_sequences(sequence, maxlen=max_length, padding='post')
-    prediction = model.predict(padded_sequence)
-    pred_index = np.argmax(prediction, axis=1)
-    pred_label = label_encoder.inverse_transform(pred_index)[0]
-    return pred_label
+    # Sua função para fazer predições aqui
+    pass
 
 # Streamlit app
 def main():
     st.title('Classificador de Reclamações')
-    model_path = '../model/model.h5'
-    tokenizer_path = '../model/tokenizer.pkl'
-    label_encoder_path = '../model/label_encoder.pkl'
+    
+    # Obtendo o diretório do script atual e construindo os caminhos completos
+    current_dir = os.path.dirname(os.path.dirname(__file__))  # Subindo um nível no diretório
+    model_path = os.path.join(current_dir, 'model', 'model.h5')
+    tokenizer_path = os.path.join(current_dir, 'model', 'tokenizer.pkl')
+    label_encoder_path = os.path.join(current_dir, 'model', 'label_encoder.pkl')
     
     model, tokenizer, label_encoder = load_components(model_path, tokenizer_path, label_encoder_path)
     
